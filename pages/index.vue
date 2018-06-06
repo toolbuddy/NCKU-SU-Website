73c837802e78e532cd9f27e0c77057eb37d88ff5
f26401004 <f26401004@gmail.com>
2018-06-07 00:41:40 +0800

    Upload the structure of porject.
    Use Nuxt.js/ExpressJS to be the backend framework.
    Use Vue.js to be the frontend framework.

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="container">
+    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <h1 class="title">
+      DEVELOPERS
+    </h1>
+    <ul class="developers">
+      <li v-for="(developer, index) in developers" :key="index" class="developer">
+        <nuxt-link :to="{ name: 'id', params: { id: index }}">
+          {{ developer.name }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  async asyncData () {
+    let { data } = await axios.get('/api/developers')
+    return { developers: data }
+  },
+  head () {
+    return {
+      title: 'Developers'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title
+{
+  margin: 30px 0;
+}
+.developers
+{
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.developer
+{
+  margin: 10px 0;
+}
+</style>
