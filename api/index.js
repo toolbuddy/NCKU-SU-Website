329d342da85d2c51ffa302e7cc7aa41dac60f615
ire33164 <ire33164@gmail.com>
2018-07-21 13:52:49 +0800

    Crush the project,fuck me please

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ const app = express();
 
 // Require API routes
 const users = require('./routes/test');
+const login = require('./routes/login');
 
 // Import API Routes
 app.use(users);
+app.use(login);
 
 // Export the server middleware
 module.exports = {
