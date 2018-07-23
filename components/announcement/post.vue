76a401fbd2204e77dcc4e61d77144fec71b138f9
f26401004 <f26401004@gmail.com>
2018-07-23 17:17:59 +0800

    Complete simple post page.
    1. use components /components/announcement/post in page/announcement/post

---
+++ components/announcement/post.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <label> 標題 Title </label> <br/> <input type="text"/> <br/>
+    <label> 發布者： {{author}} </label> <br/>
+    <time> 發布時間：{{currentTime}} </time> <br/>
+    <label> 內容 Content </label> <br/> <textarea rows="8" cols="50"> </textarea> <br/>
+    <button type="button" v-on:click="post"> 發布 Post </button>
+    <button type="button"> 取消 Cancel </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'custom-post',
+    props: {
+      author: String
+    },
+    data () {
+      return {
+        currentTime: null
+      }
+    },
+    created () {
+      this.time()
+      setInterval(this.time, 1000)
+    },
+    destroyed () {
+      clearInterval(this.time())
+    },
+    methods: {
+      time: function () {
+        const date = new Date()
+        var str = date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate() + ' '
+        var hours = date.getHours()
+        var minutes = date.getMinutes()
+        var seconds = date.getSeconds()
+        if (hours < 10) {
+          hours = '0' + hours
+        }
+        if (minutes < 10) {
+          minutes = '0' + minutes
+        }
+        if (seconds < 10) {
+          seconds = '0' + seconds
+        }
+        str += hours + ':' + minutes + ':' + seconds + ' '
+        if (hours > 11) {
+          str += 'PM'
+        } else {
+          str += 'AM'
+        }
+        this.currentTime = str
+      },
+      post: function () {
+        console.log('post button click!!')
+        // post announcement code here...
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
