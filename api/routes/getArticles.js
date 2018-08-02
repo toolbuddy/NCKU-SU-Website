cd93f1cbf4812c77f92f671e9d1f8a82be0cb8e2
ire33164 <ire33164@gmail.com>
2018-08-02 12:39:05 +0800

    Build api/routes/getArticles.js

---
+++ api/routes/getArticles.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const router = Router();
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({extends : false});
+const ArticleOp = require('../../model/query/article.js');
+
+
+router.post('/getArticle_topnews',urlencodedParser,(req , res) => {
+
+})
+
+router.post('/getArticle_message',urlencodedParser,(req,res) => {
+
+})
