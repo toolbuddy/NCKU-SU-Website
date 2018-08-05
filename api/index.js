ae07480e79d0d1657e6b2ba529bed8d603bed2d1
ire33164 <ire33164@gmail.com>
2018-08-05 17:50:18 +0800

    Set simple cookie
    Using connect-session-sequelize to save session to mysql
    You should change model/config/index.js that it connect to your database
    You can know whether it is loginning by requesting to '/' in api/index.js

--- api/index.js
+++ api/index.js
@@ -6,30 +6,45 @@ const app = express();
 // import express-session
 const session = require('express-session');
 
+//Import sequelize
+const database = require('../model/sqldb');
+const sequelize = database.sequelize;
+
 // Import connect-session-Sequelize
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// create sequelizestore
+const myStore = new SequelizeStore({
+  db: sequelize
+})
+
+// session setting
+app.use(session({
+  secret: '128 bytes random string', //will change it to another string in future
+  store: myStore,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 600 * 1000 } // 10分鐘到期 default:{path:'/',httpOnly:true,secure:false,maxAge:null}
+}))
 
 // Require API routes
 const users = require('./routes/test');
 const login = require('./routes/login');
 
+//Judge whether login or not
+app.get('/',(req,res,next) => {
+  if(req.session.isLogin){
+    console.log("online");
+  } else {
+    console.log("Unlogin");
+  }
+  req.send(req.session.isLogin); //true or false
+})
+
 // Import API Routes
 app.use(users);
 app.use(login);
 
-// create sequelizestore
-const myStore = new SequelizeStore({
-  db: 'mydb'
-})
-
-// session setting
-app.use(session({
-  secret: '128 bytes random string',
-  store: myStore,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { user: "default" , maxAge: 60 * 1000 } // 1分鐘到期 default:{path:'/',httpOnly:true,secure:false,maxAge:null}
-}))
 // Auto create session table
 myStore.sync();
 
--- api/routes/login.js
+++ api/routes/login.js
@@ -3,51 +3,46 @@ const router = Router()
 const accountOp= require('../../model/query/account.js')
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false});
-const sha1 = require('js-sha1')
-const session = require('express-session');
 
-app.get('/',(req,res)=>{
-  console.log('123');
-  res.end();
-})
-
-router.post('/user', urlencodedParser, (req, res) => {
-    let username = req.body.username;
+router.post('/login', urlencodedParser, (req, res) => {
+    var username = req.body.username;
     let pwd = req.body.password;
     let Valvalid = checkVal(pwd);
     let lengthvalid = checkLength(pwd);
-    let status;
 
   if(Valvalid && lengthvalid){
-    // connect to mysql code...
+    // connect to mysql
     accountOp.login(username, pwd)
     .then( val =>{
         // 0 -> success
         // 1 -> wrong password
         // 2 -> wrong account
-      status = "0"
-      if(status == "0"){
+      if(1){
+        // set session data
         req.session.username = username;
         req.session.isLogin = true;
+        res.send("0");
       }
     });
   }
   else if(Valvalid && (!lengthvalid)){
-    status = "100"; // invalid password's length
+    // invalid password's length
+    res.send("100");
   }
   else if((!Valvalid) && lengthvalid){
-    status = "200"; // invalid password's char
+    // invalid password's char
+    res.send("200");
   }
   else{
-    status = "300"; // invalid password's length and char
+    // invalid password's length and char
+    res.send("300");
   }
-
-  res.send(status);
 })
 
+// logout
 router.get('/logout',(req,res) => {
   req.session.destroy();
-  res.redirect('/');
+  res.end();
 })
 
 
