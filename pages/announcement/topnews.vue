0affc72cb74d5df43bd89bf56bb40b8b7967ea73
f26401004 <f26401004@gmail.com>
2018-08-15 15:05:57 +0800

    Complete the simple topnews/message page.
    1. Rename the topnews/message component name.
    2. Use the method of replacing node rather than use innerHTML directly display content.

--- pages/announcement/topnews.vue
+++ pages/announcement/topnews.vue
@@ -1,9 +1,29 @@
 <template>
-  <h1> Top News 重要公告 </h1>
+  <div>
+    <topnews-comp v-for="post in this.articles" :key="post.content" v-bind:title="post.title" v-bind:author="post.author" 
+      v-bind:time="post.title" v-bind:visitors="post.visit" v-bind:content="post.content"> </topnews-comp>
+  </div>
+
 </template>
 
 <script>
+  import TopnewsComp from '~/components/announcement/topnews-column.vue'
+
   export default {
+    data () {
+      return {
+        articles: [
+          {title: 'test', author: 'F74052104', time: '2018-08-15 08:08 AM', visit: 120, content: '<p>test for content</p>'},
+          {title: 'test2', author: 'F74052104', time: '2018-08-15 08:18 AM', visit: 240, content: '<p>test2 for content</p>'},
+          {title: 'test3', author: 'F74052104', time: '2018-08-15 08:28 AM', visit: 360, content: '<p>test3 for content</p>'}
+        ]
+      }
+    },
+    async asyncData (context) {
+    },
+    components: {
+      TopnewsComp
+    }
   }
 </script>
 
