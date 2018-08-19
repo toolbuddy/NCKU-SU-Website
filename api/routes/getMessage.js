bb565116058aeda5df14a47dc17e005909d67d80
f26401004 <f26401004@gmail.com>
2018-08-19 23:03:09 +0800

    Complete topnews fetch the data from database.
    1. Complete the part of topnews fetching the data from database.
    2. Modify the API js filename.
    3. Modify the return value method of getTopnewsSum and getMessageSum.

--- api/routes/getMessage.js
+++ api/routes/getMessage.js
@@ -5,8 +5,8 @@ const urlencodedParser = bodyParser.urlencoded({extends : false});
 const ArticleOp = require('../../model/query/article.js');
 
 router.post('/getMessage',urlencodedParser,(req,res) => {
-    let number = 1;
-    let offset = req.body.number - 1;
+    let number = 10;
+    let offset = parseInt(req.body.offset);
     ArticleOp.getArticle(0,number,offset)
     .then((val) => {
         res.json(val);
