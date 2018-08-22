fefd58e34e11b47d704b24eac37229c70b25cfee
Pei-Hsuan Hung <afcidk@gmail.com>
2018-08-22 15:27:47 +0800

    Add column to store picture path

--- model/model/account.js
+++ model/model/account.js
@@ -3,25 +3,28 @@ var bcrypt = require('bcryptjs');
 module.exports = function(sequelize, DataTypes) {
   var account = sequelize.define('account', {
     studentId: {
-      type:DataTypes.STRING(9),
+      type: DataTypes.STRING(9),
       primaryKey: true
     },
     password: {
-      type:DataTypes.STRING(60)
+      type: DataTypes.STRING(60)
     },
     permission: {
-      type:DataTypes.INTEGER,
-	    defaultValue: false
+      type: DataTypes.INTEGER,
+	  defaultValue: false
     },
     email: {
       type: DataTypes.STRING(30)
     },
     name: {
-      type:DataTypes.STRING(16)
+      type: DataTypes.STRING(16)
     },
     verified: {
-  	  type:DataTypes.BOOLEAN,
-	    defaultValue: false
+  	  type: DataTypes.BOOLEAN,
+	  defaultValue: false
+    },
+    iconPath: {
+      type: DataTypes.STRING(30)
     }
   });
 
