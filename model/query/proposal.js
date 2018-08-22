e0b293111d2a8842731d002b866a9bb6e08c9c15
Pei-Hsuan Hung <afcidk@gmail.com>
2018-08-22 15:19:08 +0800

    Add tags and classes

--- model/query/proposal.js
+++ model/query/proposal.js
@@ -4,15 +4,62 @@ const Op = db.sequelize.Op;
 const ID_FILTER = '%%%%%%%%%'
 
 function add(data) {
-  db.sequelize.transaction( t => {
-    target.create({
-      studentId: data.studentId,
-      title: data.title,
-      content: data.content,
-      needVoter: data.needVoter,
+  // Promise array
+  const pa = new Array()
+    
+  pa.push(new Promise((resolve, reject) => {
+    db.sequelize.transaction( t => {
+      target.create({
+        studentId: data.studentId,
+        title: data.title,
+        content: data.content,
+        needVoter: data.needVoter,
+        classId: data.classId
+      })
+      .then( res => {
+        resolve(res.getDataValue('id'));
+      })
+      .catch( err => {
+        reject(err);
+      });
     });
-  });
-  // TODO: class? tag?
+  }));
+
+  // push tag to promise array
+  for (var i=0; i<data.tags.length; ++i) {
+    pa.push(new Promise((resolve, reject) => {
+      const tag = data.tags[i];
+      db.sequelize.transaction( t => {
+        db.models.tag.findOrCreate({
+          where: { title: tag}
+        })
+        .then( res => {
+          resolve(res[0].getDataValue('id'));
+        })
+        .catch( err => {
+          reject(err);
+        });
+      });
+    }));
+  };
+
+  // create proposalTag
+  return Promise.all(pa)
+  .then( res => {
+    // res[0] -> article, res[1.....] -> tag
+    for (var i=1; i<res.length; ++i) {
+      db.models.proposalTag.create({
+        tagId: res[i],
+        proposalId: res[0]
+      })
+      .then( res => {
+        return true;
+      })
+      .catch( err => {
+        return false;
+      });
+    }
+  }
 }
 
 function del(id) {
@@ -24,7 +71,7 @@ function del(id) {
   });
 }
 
-function getArticle(sum, offset, id=ID_FILTER) {
+function getArticle(sum, offset, classId, id=ID_FILTER) {
   return new Promise( (resolve, reject) => {
     target.findAll({
       order: ['id'],
