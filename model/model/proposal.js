b3e54e468c483333e1af762b4040506665b80825
Pei-Hsuan Hung <afcidk@gmail.com>
2018-08-22 15:24:36 +0800

    Modify proposal model definition

--- model/model/proposal.js
+++ model/model/proposal.js
@@ -1,21 +1,23 @@
 module.exports = function(sequelize, DataTypes){
 
   var proposal = sequelize.define('proposal', {
-      title: {
-          type: DataTypes.STRING(64)
-      },
-      content: {
-          type: DataTypes.TEXT
-      },
-      needVoter: {
-          type: DataTypes.INTEGER
-      },
-      currentVoter: {
-          type: DataTypes.INTEGER
-      },
-      closeDiscuss: {
-          type: DataTypes.BOOLEAN
-      }
+    title: {
+        type: DataTypes.STRING(64)
+    },
+    content: {
+        type: DataTypes.TEXT
+    },
+    threshold: {
+        type: DataTypes.INTEGER
+    },
+    currentVoter: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    },
+    closeDiscuss: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
   });
 
   return proposal;
