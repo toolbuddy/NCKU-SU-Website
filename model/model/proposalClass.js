6ac72a945b552f2086259bb8718901a72895f94e
Pei-Hsuan Hung <afcidk@gmail.com>
2018-08-25 23:10:43 +0800

    Update model definition

--- model/model/proposalClass.js
+++ model/model/proposalClass.js
@@ -1,11 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
-    var proposalClass = sequelize.define('proposalClass', {
-      title: {
-          type: DataTypes.STRING(16)
-      }
-    }, {
-        timestamps: false
-    });
-
-    return proposalClass;
+  var proposalClass = sequelize.define('proposalClass', {
+    title: {
+        type: DataTypes.STRING(16)
+    }
+  }, {
+      timestamps: false
+  });
+  
+  proposalClass.associate = function(models) {
+    proposalClass.hasMany(models.proposal, {foreignKey: 'classId'});
+  }
+  
+  return proposalClass;
 };
---
+++ model/model/topNews.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+    var topNews= sequelize.define('topNews', {
+        title: {
+            type: DataTypes.STRING
+        },
+        content: {
+            type: DataTypes.TEXT
+        }
+    });
+
+	topNews.associate = function(models) {
+		topNews.belongsToMany(models.tag, {through: models.articleTag, foreignKey: 'newsId'});
+		topNews.belongsToMany(models.account, {through: models.collection, foreignKey: 'topNewsId'});
+	}
+
+    return topNews;
+}
