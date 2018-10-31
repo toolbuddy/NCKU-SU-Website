11a60b13acb0b20d99057863de87768659c0f083
travischen0219 <travis.xin@gmail.com>
2018-10-31 22:43:43 +0800

    Add many ui-features.

--- components/header.vue
+++ components/header.vue
@@ -1,17 +1,22 @@
 <template>
   <header>
-    <h2><router-link :to="{path: '/'}"> 第 26 屆學生會官方網站 </router-link></h2>
+    <div class="bg">
+    <h2>成大學生會</h2>
+
+    <section class="float_right">
     <ul class="custom-ul-style">
-      <li><a href="#"> 關於學生會 </a></li>
       <li><a href="#"> 文章總覽 </a></li>
       <li><a href="#"> 公共政策參與平台 </a></li>
-      <li><a href="#"> 聯絡我們 </a></li>
+      <li><a href="#"> 關於學生會 </a></li>
+      <li><a href="#"> 聯絡我們   </a></li>
     </ul>
-    <ul class="custom-ul-style float-right">
+    </section>
+    <!-- <ul class="custom-ul-style float-right">
       <li><router-link :to="{path: '/account/login'}"> 登入 </router-link></li>
       <li><a href="#"> 臉書 </a></li>
       <li><a href="#"> IG </a></li>
-    </ul>
+    </ul> -->
+    </div>
   </header>
 </template>
 
@@ -22,34 +27,46 @@
 </script>
 
 <style scoped>
+  .bg {
+    height:53px;
+    width:100%;
+    position: fixed;
+    background-color: #FFFFFF;
+    opacity: 0.5;
+    z-index: 69;
+  }
+  .float_right {
+    padding-right: 20px;
+    float:right;
+  }
   h2 {
+    padding-left: 20px;
+    color: #09A6AA;
+    font-size: 23px;
     display: inline;
+    text-decoration:none;
   }
   ul {
     display: inline;
   }
   ul.custom-ul-style {
-    padding: 10px 16px;
+    font-size: 23px;
     list-style-type: none;
   }
   ul.custom-ul-style li {
     display: inline;
   }
   ul.custom-ul-style li+li:before {
-    padding: 8px;
-    color: black;
-    content: "/\00a0";
+    padding: 20px;
+    color: #09A6AA;
+    content: " ";
   }
   ul.custom-ul-style li a {
-    color: #0275d8;
+    color: #09A6AA;
     text-decoration: none;
   }
   ul.custom-ul-style li a:hover {
-    color: #01447e;
-    text-decoration: underline;
+    color: #09A6AA;
+    text-decoration:none;
   }
-
-  .float-right {
-    float: right;
-  }
-</style>
+  </style>
